32a1182376e3308100496a3543454830771817e5
CherryIce <a13530836164@163.com>
2017-11-27 14:35:02 +0800

    Initial Commit

---
+++ UILocalizedIndexedCollation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UILocalizedIndexedCollation
+//
+//  Created by Macx on 2017/11/27.
+//  Copyright © 2017年 Macx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UILocalizedIndexedCollation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UILocalizedIndexedCollation
+//
+//  Created by Macx on 2017/11/27.
+//  Copyright © 2017年 Macx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
